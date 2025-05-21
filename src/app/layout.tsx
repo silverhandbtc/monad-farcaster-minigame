a49ff7edb2b0aaacbd55d61e5cfbd9cb2d8cc218
silverhandbtc <santiagosilverhand@gmail.com>
2025-05-20 23:11:26 -0300

    feat: add base pages

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { FrameProvider } from "@/components/farcaster-provider";
+
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Monad Farcaster MiniApp Template",
+  description: "A template for building mini-apps on Farcaster and Monad",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <FrameProvider>{children}</FrameProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/components/pages/app.tsx
@@ -0,0 +1,18 @@
+import { SafeAreaContainer } from "@/components/safe-area-container";
+import { useMiniAppContext } from "@/hooks/use-miniapp-context";
+import dynamic from "next/dynamic";
+import { Button } from "../ui/button";
+
+const Demo = dynamic(() => import("@/components/Home"), {
+  ssr: false,
+  loading: () => <div>Loading...</div>,
+});
+
+export default function Home() {
+  const { context } = useMiniAppContext();
+  return (
+    <SafeAreaContainer insets={context?.client.safeAreaInsets}>
+      <Demo />
+    </SafeAreaContainer>
+  );
+}
